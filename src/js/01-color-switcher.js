cd3e21651d0d8fca00ba7af2464a68b069bbfd8a
YuriyShevchyk <97519385+YuriyShevchyk@users.noreply.github.com>
2022-08-15 21:26:03 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const startBtn = document.querySelector(`[data-start]`);
+const stopBtn = document.querySelector(`[data-stop]`);
 
+startBtn.addEventListener(`click`, startColorizer);
+stopBtn.addEventListener(`click`, stopColorizer);
+
+var btnIsActive = false;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function startColorizer() {
+    
+    btnIsActive = false
+    startBtn.setAttribute('disabled', '');
+    startBtn.classList.remove(".button-switcher:hover")
+    stopBtn.removeAttribute('disabled');
+    const intervalId = setInterval(() => {
+        if (btnIsActive === true) {
+            clearInterval(intervalId);
+            console.log("interval cleared")
+            return
+        }
+        const randomColor = getRandomHexColor()
+        document.body.style.backgroundColor = randomColor
+    
+    }, 1000)
+    
+}
+
+function stopColorizer() {
+    btnIsActive = true
+    console.log("stop click")
+    startBtn.removeAttribute('disabled');
+    stopBtn.setAttribute('disabled', '');
+}
