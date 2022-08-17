68fd4d185c455bddd10eb2b8302725bcfdc1bee8
YuriyShevchyk <97519385+YuriyShevchyk@users.noreply.github.com>
2022-08-18 01:23:31 +0300

    2_task_start

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,6 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -15,4 +18,33 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
+}
+
+const daysEl = document.querySelector("[data-days]");
+const hoursEl = document.querySelector("[data-hours]");
+const minutesEl = document.querySelector("[data-minutes]");
+const secondsEl = document.querySelector("[data-seconds]");
+
+const inputEl = document.querySelector("#datetime-picker");
+const timerEl = document.querySelector(".timer");
+const startBtn = document.querySelector("[data-start]");
+
+
+startButton.addEventListener('click', onStart)
+inputEl.addEventListener('input', onInput)
+
+
+const countDate = flatpickr("#datetime-picker", {
+    enableTime: true,
+    dateFormat: "Y-m-d H:i",
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+})
+
+function pad(value) {
+    return String(value).padStart(2, '0')
 }
